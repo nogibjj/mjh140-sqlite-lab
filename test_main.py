fbdff46357f6869e9b4175ccc3230ba05040a9fd
Matthew Holden <matthew.holden86@gmail.com>
2023-10-02 17:06:16 -0400

    added unit testing

--- test_main.py
+++ test_main.py
@@ -1,10 +1,41 @@
 """
-Test goes here
-
+Unit Testing
 """
 
-from mylib.calculator import add
+import unittest
+#import sys
+#import os
 
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+#sys.path.insert(0, parent_dir)
 
-def test_add():
-    assert add(1, 2) == 3
+from mylib.extract import extract
+from mylib.query import query
+from mylib.transform_load import load
+
+class TestLib(unittest.TestCase):
+    def test_correct_path(self):
+        try:
+            extract(url="https://raw.githubusercontent.com/jfinocchiaro/marchmadness/master/kenpom.csv", 
+                    file_path = "kenpom.csv")
+        except:
+            self.fail("Path Failure")
+    
+    def test_query(self):
+        try:
+            query(dbname = "kenpom.db",
+                  table = 'kenpom_data')
+        except:
+            self.fail("Query Failure")
+    
+    def test_load(self):
+        try:
+            load(dataset="kenpom.csv",
+                 database="kenpom.db",
+                 target_table = "kenpom_data")
+        except:
+            self.fail("Load Failure")
+
+if __name__ == '__main__':
+    unittest.main()
